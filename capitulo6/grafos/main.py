9c4a028569ce5ae8492ad84762134d0beb14cdf5
Luan de Oliveira <luan012012@hotmail.com>
2024-06-05 23:43:03 -0300

    feat: adicionado grafo

---
+++ capitulo6/grafos/main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    grafo = {}
+    grafo['você'] = ["Alice", "Bob", "Claire"]
+    grafo['Alice'] = ["Peggy"]
+    grafo['Bob'] = ["Anuj"]
+    grafo['Claire'] = ["Thom", "Jonny"]
+    grafo['Peggy'] = []
+    grafo['Anuj'] = []
+    grafo['Thom'] = []
+    grafo['Jonny'] = []
+
+    print(grafo)  # Press ⌘F8 to toggle the breakpoint.
